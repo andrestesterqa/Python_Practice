2d9a2088c373bd77d63e149a1a989104b20ad4a2
andrestesterqa <andrestesterqa@gmail.com>
2020-07-26 13:54:59 -0300

    Agregando p´ractica de Operadores Lógicos

---
+++ operadores_logicos.py
@@ -0,0 +1,17 @@
+# Operadore lógicos "and" y "or"
+
+a = 2
+b = 5
+
+if a == 2:
+    print("Valor correcto")
+
+if a == 2 and b == 3:
+    print("Los valores son correctos")
+else:
+    print("Los valores son incorrectos")
+
+if a == 2 or b ==6:
+    print("Los valores son correctos")
+else:
+    print("Los valores son incorrectos")
