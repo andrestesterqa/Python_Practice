f275393391d416f30d4be540dde49b114c779123
andrestesterqa <andrestesterqa@gmail.com>
2020-07-26 13:56:32 -0300

    Práctica de Multi Elif

---
+++ multy_elif.py
@@ -0,0 +1,11 @@
+resultado = int(input("Cual número salió en el dado? "))
+if resultado == 1:
+    print("Ganaste un cofre!")
+elif resultado == 2:
+    print("Ganastes una gema")
+elif resultado == 3:
+    print("Ganaste una vara mágica")
+elif resultado == 4:
+    print("Ganaste el premio MAYOR")
+elif resultado == 5:
+    print("No ganastes NADA")
